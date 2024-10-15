814f5d77cb08ae43e6a60f02ce1973b754b66b8c
Ihor kandyba <ihor.kandyba@chmnu.edu.ua>
2024-10-15 14:30:24 +0300

    add new func

---
+++ new_file.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+//супер важливий код
+int main (){
+    cout<<"hello";
+    return 0;
+}
+int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}int main (){
+    cout<<"hello";
+    return 0;
+}
