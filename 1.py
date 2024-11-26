82343e4797e5f0680f40858e61d35cc73ecd91e5
Ihor kandyba <ihor.kandyba@chmnu.edu.ua>
2024-11-26 14:41:52 +0200

    add tag_1 array

--- 1.py
+++ 1.py
@@ -4,6 +4,7 @@ import tabulate
 class Lexer:
     def __init__(self):
         self.tags=['body','p','img','a', 'h1','h2','table','tr','td']
+        self.tags_1=['body','p','img','a', 'h1','h2','table','tr','td']
         self.attribute=['align','width','img','border', 'src','alt']
         self.ids=r'[0-9A-Za-z_]+'
     def is_equel_symb(self,inpt_str):
@@ -16,8 +17,6 @@ class Lexer:
         return inpt_str.lower() in self.tags
     def is_attribute(self,inpt_str):
         return inpt_str in self.attribute
-    def is_quotes(self,inpt_str):
-        return inpt_str=="\'" or inpt_str=="\""
     def is_val_name(self,inpt_str):
         return re.fullmatch(self.ids,inpt_str) is not None and not (self.is_tag(inpt_str) or self.is_attribute(inpt_str))
     
